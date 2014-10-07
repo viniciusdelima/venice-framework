e6d51a761ab1898b7680bebc4572fa5f7e6e0c9a
Vinicius Lima <vinicius.c.lima03@gmail.com>
2014-10-07 00:16:50 -0300

    Nova estrutura

---
+++ app/includes/config/module_alias.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Este arquivo têm por objetivo aramazenar as configurações sobre os alias dos módulos.
+ * Os alias são nomes pseudônimos adicionais que podem ser usados em conjunto com um Módulo para
+ * que a URL possa ser personalizada sem a necessidade de levar o nome do módulo e action em uso.
+ * Para exemplificação com o uso de um alias para o módulo user, a URL que antes atendia pelo endereço:
+ * http://www.pidigital.com.br/admin/user/login
+ * Pode atender pelo endereço: 
+ * http://www.pidigital.com.br/admin/usuario/logar
+ * caso os alias logar e usuario tenham sido definidos para o módulo user.
+ */
+
+$config = array(
+	'usuario' => array(
+		'user', array(
+			'home' => 'index',
+			'login' => 'login',
+			'sair' => 'logout'		
+		)		
+	),
+	'painel' => array(
+		'dashboard', array(
+			'home'     => 'index',	
+			'cadastro' => 'register'	
+		)	
+	),
+	'formulario-cadastro' => array(
+		'registerform', array(
+				'home'     => 'index',
+				'atualiza' => 'update'
+		)
+	),
+	'formulario-contato' => array(
+		'contactform', array(
+				'home'     => 'index',
+				'atualiza' => 'update'
+		)
+	),
+	'pagina' => array(
+		'page', array(
+			'home'     => 'index',
+			'adiciona' => 'add',
+			'listar'   => 'listall'
+		)
+	)
+);
