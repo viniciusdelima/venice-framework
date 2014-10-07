f178482c602b7d4a77993d149728454142e67b3d
Vinicius Lima <vinicius.c.lima03@gmail.com>
2014-10-07 00:38:57 -0300

    Nova estrutura

--- app/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start();
-
-/** Inicia o sistema **/
-require 'vf-init.php';
-
-/** Carrega o sistema, processa a requisição e renderiza a página **/
-require 'vf-load.php';
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+ob_start();
+
+/** Inicia o sistema **/
+require 'vf-init.php';
+
+/** Carrega o sistema, processa a requisição e renderiza a página **/
+require 'vf-load.php';
